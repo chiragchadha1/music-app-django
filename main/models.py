3fd1397ac43d9d52a2fe3d843216d82dcbfce5b4
francesco valenti <francescovalenti664@gmail.com>
2023-11-29 16:21:13 +0100

    Updated models

--- main/models.py
+++ main/models.py
@@ -1,4 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
-#hello world
+
+class Artist(models.Model):
+    
+    name=models.CharField(max_length=30)
+    followers=models.ManyToManyField(User)
+    
+    def __str__(self):
+        
+        string=f"{self.name}"
+        return string
+    
+class Song(models.Model):
+    
+    title=models.CharField(max_length=30)
+    duration=models.DurationField()
+    language=models.CharField(max_length=5)
+    releaseDate=models.DateField()
+    album=models.CharField(max_lenght=30)
+    artists=models.ManyToManyField(Artist)
+    
+    def __str__(self):
+        
+        string=f"{self.title}"
+        return string
+    
+class Playlist(models.Model):
+    
+    name=models.CharField(max_length=20)
+    duration=models.DurationField()
+    songs=models.ManyToManyField(Song)
+    creator=models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        
+        string=f"{self.name}"
+        return string
+    
+    
+    
